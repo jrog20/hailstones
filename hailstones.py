602a23bbe725be2c328d521367a6a68f22cc7380
Jaime Rogstad <66330786+jrog20@users.noreply.github.com>
2021-04-28 18:46:57 -0400

    Update hailstones.py

--- hailstones.py
+++ hailstones.py
@@ -9,15 +9,15 @@ def main():
 
     while n != 1:
         if n % 2 == 0:
-            print(str(n) + ' is even, so I take half: ' + str(int(n/2)))
+            print(n, 'is even, so I take half:', n//2)
             n //= 2
             count += 1
         else:
-            print(str(n) + ' is odd, so I make 3n + 1: ' + str(n * 3 + 1))
+            print(n, 'is odd, so I make 3n + 1:', n * 3 + 1)
             n = (n * 3) + 1
             count += 1
 
-    print('This process took ' + str(count) + ' steps to reach 1')
+    print('This process took', count, 'steps to reach 1')
 
 
 if __name__ == '__main__':
